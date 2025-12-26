dca65431a9b4b8a173deb5fa2fca42e0f3d63a03
HehCrashes <3095217291@qq.com>
2025-12-26 16:11:36 +0800

    点与弧

--- src/main/java/com/github/hehcrashes/simutils/magic_circle/res/IRenderable.java
+++ src/main/java/com/github/hehcrashes/simutils/magic_circle/res/IRenderable.java
@@ -3,5 +3,6 @@ package com.github.hehcrashes.simutils.magic_circle.res;
 import javafx.scene.canvas.GraphicsContext;
 
 public interface IRenderable {
-    void render(GraphicsContext gc, double cx, double cy,double scale, int cent);
+    void render(GraphicsContext gc,double r, double cx, double cy,double scale);
 }
+
---
+++ src/main/java/com/github/hehcrashes/simutils/magic_circle/res/IRenderableArc.java
@@ -0,0 +1,7 @@
+package com.github.hehcrashes.simutils.magic_circle.res;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public interface IRenderableArc {
+    void render(GraphicsContext gc, double r, double cx, double cy, double beginAngle, double allAngle, double scale);
+}
